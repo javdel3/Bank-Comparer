95aec244ba27f5fa3a9c42e76caf217643752850
Javier  Delgado <122196793+javdel3@users.noreply.github.com>
2025-12-18 07:04:24 +0000

    logic change - input error handling

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "headers.h"
 #include <string>
 #include <limits> //Library to help check if user correctly input integer
+#include <sstream>
 using namespace std;
 
 class Bank {
@@ -13,6 +14,7 @@ class Bank {
 
 int main() {
   //Variables for user input
+  string input = "";
   double user_principle = 0.00;
   unsigned int user_numYearsWithBanks = 0;
   bool isValidBankName = false;
@@ -27,7 +29,6 @@ int main() {
   user_Bank2.apy = 0.00;
   user_Bank2.totalMoneyEarned = 0.00;
 
-
   PrintStartDisplay();
   cout<<endl;
   
@@ -36,24 +37,27 @@ int main() {
     //Check for valid inputs
   do {
     //Attempt to read a double number
-    if (cin >> user_principle && user_principle > 0.00) {
+    getline(cin, input);
+    stringstream ss(input); //Initializes the stream with 'input' string as its input buffer
+
+    if(ss >> user_principle && user_principle > 0.00 && ss.eof()) {
         break;
     }
 
     cout << "---Invalid input. Please enter only positive numbers.---" << endl;
     cout << "To begin enter the Principle going to be used to compare between both Banks: ";
 
-    //Clear the error flags on the cin stream
-    cin.clear();
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
   } while (true);
+  cout<<endl;
 
   //Get user number of years planning to invest the money
   cout << "Please enter the Number of Years you plan to invest the money: "; //Use years for both banks' calculations
     //Check for valid inputs
   do {
     //Attempt to read an integer
-    if (cin >> user_numYearsWithBanks && user_numYearsWithBanks > 0) {
+    getline(cin, input);
+    stringstream ss(input); //Initializes the stream with 'input' string as its input buffer
+    if (ss >> user_numYearsWithBanks && user_numYearsWithBanks > 0 && ss.eof()) {
         //Successfully read a number so break the loop
         break;
     }
@@ -61,10 +65,8 @@ int main() {
     cout << "---Invalid input. Please enter only positive numbers.---" << endl;
     cout << "Please enter the Number of Years you plan to invest the money: ";
 
-    //Clear the error flags on the cin stream
-    cin.clear();
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
   } while (true);
+  cout<<endl;
 
   //List of available banks
   cout<<endl;
@@ -72,19 +74,13 @@ int main() {
   cout<<endl;
 
   //Bank 1 Name
-  // cout<<"Enter a valid name for bank 1 from the list above: ";
-  // cin>>user_Bank1.name;
-  // isValidBankName = CheckValidBankName(user_Bank1.name);
     //Check valid input
   while (true) {
     cout << "Enter a valid name for bank 1 from the list above: ";
-    cin >> user_Bank1.name;
-    //debug
-    cout << "Comparing [" << user_BankName << "] with [" << validBankName << "]" << endl;
-
+    getline(cin, user_Bank1.name);
 
     if (CheckValidBankName(user_Bank1.name)) {
-        break; // valid input → move on
+        break;
     }
 
     cout << "---Invalid bank name. Please try again.---" << endl;
@@ -97,7 +93,9 @@ int main() {
     //Check for valid inputs
   do {
     //Attempt to read a double number
-    if (cin >> user_Bank1.apy && user_Bank1.apy > 0.00) {
+    getline(cin, input);
+    stringstream ss(input); //Initializes the stream with 'input' string as its input buffer
+    if (ss >> user_Bank1.apy && user_Bank1.apy > 0.00 && ss.eof()) {
         //Successfully read a number so break the loop
         break;
     }
@@ -106,9 +104,6 @@ int main() {
     cout<<"Enter the APY advertised for " << user_Bank1.name << " :" << endl;
     cout<<"(Please enter only a number. Do NOT express as a precentage. Ex. enter 3.50)"<<endl;
 
-    //Clear the error flags on the cin stream
-    cin.clear();
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
   } while (true);
   cout << "%" << endl;
   cout<<endl;
@@ -136,6 +131,9 @@ int main() {
     //Check for valid inputs
   do {
     //Attempt to read a double number
+    getline(cin, input);
+    stringstream ss(input); //Initializes the stream with 'input' string as its input buffer
+
     if (cin >> user_Bank2.apy && user_Bank2.apy > 0.00) {
         //Successfully read a number so break the loop
         break;
@@ -145,9 +143,6 @@ int main() {
     cout<<"Enter the APY advertised for " << user_Bank2.name << " :" << endl;
     cout<<"(Please enter only a number. Do NOT express as a precentage. Ex. enter 3.50)"<<endl;
 
-    //Clear the error flags on the cin stream
-    cin.clear();
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
   } while (true);
   cout << "%" << endl;
   cout<<endl;
